00d1969dab81dc9cb18c5bd59cc6255583d32b72
user1 <pranierb@gmail.com>
2022-11-08 17:03:07 +0100

    Factorisation de l'entête

--- index.php
+++ index.php
@@ -1,43 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Accueil - Vercors Music Festival 2022</title>
-
-  <!-- Inclusion du Favicon du site -->
-  <link rel="icon" type="image/x-icon" href="images/favicon-VMF.svg">
-  <!-- Inclusion du CSS de Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
-  <style>
-    /* CSS custom */
-    :root {
-      --bs-border-width: 2px;
-    }
-
-    .donnee-bdd {
-      color: #427b73;
-      font-size: 1.1em;
-    }
-
-    .gras {
-      font-weight: bold;
-    }
-
-    a.lien-card {
-      color: var(--bs-body-color);
-      text-decoration: none;
-    }
-
-    label.required:after {
-      content: " *";
-      color: red;
-    }
-  </style>
-</head>
+<?php
+  require_once('head.php');
+?>
 
 <body>
   <!-- En-tête avec barre de navigation -->
