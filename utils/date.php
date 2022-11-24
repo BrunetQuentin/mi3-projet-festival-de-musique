2c551b8aac63b31023496f8f753e632e24c4e2bc
user1 <pranierb@gmail.com>
2022-11-24 08:59:43 +0100

    Sécurité de index.php par rapport aux éléments reçus et majuscules au début des 3 jours de concerts

--- utils/date.php
+++ utils/date.php
@@ -2,12 +2,15 @@
 
     /**
      * Convertis une date en format français.
-     * @param string $date Date au format YYYY-MM-DD
-     * @param string $pattern Format de la date à retourner
-     * @return string date au format français
+     * @param string $date Date au format YYYY-MM-DD.
+     * @param string $pattern Format de la date à retourner.
+     * @return string|bool Date au format français ou false en cas d'erreur.
      */
-    function dateToFrench(string $date, string $pattern): string {
+    function dateToFrench(string $date, string $pattern): string|bool {
         $dateObj = new DateTime($date);
+        if ($dateObj === false) {
+            return false;
+        }
 
         $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::FULL);
         $formatter->setPattern($pattern);
